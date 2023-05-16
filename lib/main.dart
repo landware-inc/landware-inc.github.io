509f466d9fb21baa4c2c08247121fd44d7a5a723
SM Kim <32336852+kegos@users.noreply.github.com>
2023-05-16 19:43:33 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -10,18 +11,34 @@ import 'package:kakao_login_test/screens/main_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(nativeAppKey: '45ce62ef8ac5361d8994f2a9ca50a1ed');
-  KakaoSdk.init(javaScriptAppKey: kakaoJsKey);
-  await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: "AIzaSyCWyKObSmj6xrjVqwYpI-vzsNGkLfi-2-I",
-        authDomain: "smilerealestate-e93fd.firebaseapp.com",
-        projectId: "smilerealestate-e93fd",
-        storageBucket: "smilerealestate-e93fd.appspot.com",
-        messagingSenderId: "600825662425",
-        appId: "1:600825662425:web:05a4d055d272c0c45b27c8",
-          measurementId: "G-TM4G73DX95"
-      )
-  );
+  if(kIsWeb) {
+    KakaoSdk.init(javaScriptAppKey: kakaoJsKey);
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyCWyKObSmj6xrjVqwYpI-vzsNGkLfi-2-I",
+            authDomain: "smilerealestate-e93fd.firebaseapp.com",
+            projectId: "smilerealestate-e93fd",
+            storageBucket: "smilerealestate-e93fd.appspot.com",
+            messagingSenderId: "600825662425",
+            appId: "1:600825662425:web:05a4d055d272c0c45b27c8",
+            measurementId: "G-TM4G73DX95"
+        )
+    );
+  } else {
+    await Firebase.initializeApp(
+        name: 'SmileRealEstate',
+        options: FirebaseOptions(
+            apiKey: "AIzaSyCWyKObSmj6xrjVqwYpI-vzsNGkLfi-2-I",
+            authDomain: "smilerealestate-e93fd.firebaseapp.com",
+            projectId: "smilerealestate-e93fd",
+            storageBucket: "smilerealestate-e93fd.appspot.com",
+            messagingSenderId: "600825662425",
+            appId: "1:600825662425:web:05a4d055d272c0c45b27c8",
+            measurementId: "G-TM4G73DX95"
+        )
+    );
+  }
+
 
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
