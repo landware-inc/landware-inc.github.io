d97d84679648f77228af0615afe6492e5f660954
DESKTOP-DPLIS5K\Sung H Kim <hoony0916@gmail.com>
2024-04-07 13:28:09 -0700

    System migration : new server

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,9 +59,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c0794b3bf4f7f764ea507de5b75079ee
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "12b408fec27cd9978ae3d963a4ee38f7",
-"/": "12b408fec27cd9978ae3d963a4ee38f7",
-"main.dart.js": "6371210994e78cdb615290b95b277eae",
+"index.html": "a3040e783c1b767df34987a8bc34dc5a",
+"/": "a3040e783c1b767df34987a8bc34dc5a",
+"main.dart.js": "0a5de73b9b9b7f07cf80d5272c1fe2fd",
 "manifest.json": "a1e6009655aa2138c1cbd9017e523a28",
 "version.json": "abb438ce2b69abb282636c6b5c19827a"};
 // The application shell files that are downloaded before a service worker can
